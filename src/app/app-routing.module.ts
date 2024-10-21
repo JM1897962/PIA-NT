4c4391275e48f42cc8da03228111a58518499b74
JM <1897962.jmmr@gmail.com>
2024-10-21 15:29:31 -0600

    Cuarta modificacion, creacion de los apartados restantes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,22 @@ const routes: Routes = [
     path: 'menu',
     loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
   },
+  {
+    path: 'compras',
+    loadChildren: () => import('./compras/compras.module').then( m => m.ComprasPageModule)
+  },
+  {
+    path: 'cartera',
+    loadChildren: () => import('./cartera/cartera.module').then( m => m.CarteraPageModule)
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'ventas',
+    loadChildren: () => import('./ventas/ventas.module').then( m => m.VentasPageModule)
+  },
 ];
 
 @NgModule({
