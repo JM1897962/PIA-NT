daf7c159ef428417bf09da55d21a3354838ce52d
JM <1897962.jmmr@gmail.com>
2024-10-28 12:32:10 -0600

    Actualizacion masiva

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.democapacitor.fcfm',
+  appName: 'PIA-1897962',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
